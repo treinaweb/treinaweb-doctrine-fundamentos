c220c254cd7f91dffc25fa922b18495005ee3efb
Elton Fonseca <elton869@gmail.com>
2019-10-23 17:34:11 -0300

    04 - configurando a conexão do doctrine com banco

--- index.php
+++ index.php
@@ -2,4 +2,23 @@
 
 require_once "vendor/autoload.php";
 
-phpinfo();
+use Doctrine\DBAL\DriverManager;
+use Doctrine\DBAL\Configuration;
+
+$params = [
+    'dbname' => 'doctrine',
+    'user' => 'homestead',
+    'password' => 'secret',
+    'host' => '192.168.10.10',
+    'driver' => 'pdo_mysql'  
+];
+
+$url = [
+    'url' => 'mysql://homestead:secret@192.168.10.10/doctrine'
+];
+
+$config = new Configuration;
+
+$conn = DriverManager::getConnection($params, $config);
+
+var_dump($conn);
