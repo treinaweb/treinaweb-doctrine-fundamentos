53ab87bc6008e36972a43683eb68b86bf684423b
Elton Fonseca <elton869@gmail.com>
2019-10-25 16:56:56 -0300

    10 - Mapeando a entidade

--- src/Entities/User.php
+++ src/Entities/User.php
@@ -2,10 +2,23 @@
 
 namespace App\Entities;
 
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
 class User {
 
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue
+     */
     private $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
     private $name;
 
 }
