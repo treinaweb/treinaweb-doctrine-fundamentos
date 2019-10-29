7cdcd851e8a6acbd38da1fb809bef1529363e8ea
Elton Fonseca <elton869@gmail.com>
2019-10-28 23:21:19 -0300

    17 - Inserindo dados via ORM

--- create-user.php
+++ create-user.php
@@ -2,4 +2,15 @@
 
 require_once "bootstrap.php";
 
-var_dump($entityManager);
+$user = new App\Entities\User;
+$user->setName('Judas');
+
+$user2 = new App\Entities\User;
+$user2->setName('Uriel');
+
+$entityManager->persist($user);
+$entityManager->persist($user2);
+
+$entityManager->flush();
+
+//var_dump($entityManager);
