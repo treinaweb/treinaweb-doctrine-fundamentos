03e4542b2d594e6521f21be4852e4f6b900a1062
Elton Fonseca <elton869@gmail.com>
2019-10-28 23:12:09 -0300

    16 - Configurar o leitor de Annotation

--- bootstrap.php
+++ bootstrap.php
@@ -7,7 +7,7 @@ use Doctrine\ORM\EntityManager;
 
 $paths = [__DIR__."/src/Entities"];
 $isDevMode = true;
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, null, false);
 
 $params = [
     'url' => "mysql://homestead:secret@192.168.10.10/doctrine"
